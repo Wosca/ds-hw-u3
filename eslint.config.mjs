d055dcb870734c0b2ba499d5aa0998c863b94cbe
Oscar <83197788+Wosca@users.noreply.github.com>
2025-03-18 18:09:41 +1000

    Fixed eslint ignore list

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const compat = new FlatCompat({
 });
 
 export default defineConfig([
-  globalIgnores(["app/components/ui/*", ".next", "node_modules"]),
+  globalIgnores(["components/ui/*", ".next", "node_modules"]),
   {
     extends: compat.extends(
       "next/core-web-vitals",
